89439218c0ca27e7f7494659b4f839cd2b28e422
justin <justinglobal@gmail.com>
2016-03-17 10:36:12 -0700

    madlib game v2

---
+++ 2-madlib2.py
@@ -0,0 +1,8 @@
+print('This is a madlib game.')
+print(' What is your favorite color')
+color = input()
+print('What is your preferred mode of transportation?')
+transport = input()
+print('What is your favorite holiday?')
+holiday = input()
+print('You will receive a ' + color + ' ' + transport + ' on ' + holiday)
